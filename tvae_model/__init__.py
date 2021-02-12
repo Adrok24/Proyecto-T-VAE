8373e78eb2102858ba4a01bd62de7bab1d8f8a7b
Adrian Di Paolo <adriandipaolo@gmail.com>
2021-02-12 17:26:42 -0300

    Add imports to __init__

--- tvae_model/__init__.py
+++ tvae_model/__init__.py
@@ -1,3 +1,12 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Layer, Dense, LSTM, Bidirectional, TimeDistributed
+import tensorflow.keras.backend as K
+import pandas as pd
 from .multiheadattention import MultiHeadAttention
 from .positional_encoding import PositionalEncoding
 from .sampling import Sampling
