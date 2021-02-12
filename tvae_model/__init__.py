9262710cf8bab668ecf96da3f44c5a2c1200c9d5
Adrian Di Paolo <adriandipaolo@gmail.com>
2021-02-12 17:20:17 -0300

    Add resourse in __init__ file

--- tvae_model/__init__.py
+++ tvae_model/__init__.py
@@ -2,3 +2,4 @@ from .multiheadattention import MultiHeadAttention
 from .positional_encoding import PositionalEncoding
 from .sampling import Sampling
 from .vae_model import VaeModel
+from .utils import *
